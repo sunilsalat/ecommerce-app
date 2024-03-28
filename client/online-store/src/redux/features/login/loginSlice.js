652afd27c66e734ba344ac958bcc943a63bbca07
Sunil Salat <sunil.salat@reynasolutions.com>
2024-03-28 18:33:10 +0530

    redux setup

---
+++ client/online-store/src/redux/features/login/loginSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+import Login from "../../../pages/login";
+
+export const loignSlice = createSlice({
+    name: Login,
+    initialState: {},
+    reducers: {},
+    extraReducers: {},
+});
